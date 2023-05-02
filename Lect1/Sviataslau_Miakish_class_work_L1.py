f8bd2349fb1e7b44c6510e27bafb4b092f0c7f38
Lordov <lidermtut1@gmail.com>
2023-05-02 15:35:25 +0300

    ADD Homework Lect1

---
+++ Lect1/Sviataslau_Miakish_class_work_L1.py
@@ -0,0 +1,64 @@
+# # Class Work 1.0
+
+# print("The itsy bitsy spider\nclimbed up the waterspout.")
+# print()
+# print("Down came the rain\nand washed the spider out.")
+
+# print("\n\n\n\n\n\nn")
+
+# print('\\\\\\\\\\\\')
+
+
+# print("The itsy bitsy spider", "climbed up", "the waterspout.")
+
+# print("My name is", "Pyhton.", end=" ")
+# print("Monty Pyhton.")
+
+# print("My", "name", "is", "Monty", "Pyhton", sep="-")
+
+# print("My", "name", "is", sep="_", end="*")
+# print("Monty", "Pyhton", sep="*", end="*\n")
+
+# Окталы
+
+print(0o231)
+print(0o123)
+
+# Дробные числа.
+print(2.5)
+print(-0.4)
+print(.4)
+
+print(2e2)
+
+# Строки.
+
+print("""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
+
+# Назначение переменных. 
+
+b = 0
+my_wallet_after_course = 10000
+
+print(my_wallet_after_course)
+
+# Вычисления.
+
+my_wallet_after_course += 20000
+print(my_wallet_after_course)
+
+import math
+a=55.63523
+
+print(round(a, 2))
+
+print(math.floor(a))
+print(math.ceil(a))
